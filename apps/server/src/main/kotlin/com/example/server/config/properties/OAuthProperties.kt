b3b41c84344547828f7ddb84201c3c0c22113f51
MinChanJu <mcj00220077@gmail.com>
2026-07-30 00:43:33 +0900

    chore: KAN-17-#17 OAuthConfig 및 property 이름 변경
    - Oauth -> OAuth

--- apps/server/src/main/kotlin/com/example/server/config/properties/OAuthProperties.kt
+++ apps/server/src/main/kotlin/com/example/server/config/properties/OAuthProperties.kt
@@ -3,6 +3,6 @@ package com.example.server.config.properties
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "oauth")
-data class OauthProperties(val providers: Map<String, ProviderConfig>) {
+data class OAuthProperties(val providers: Map<String, ProviderConfig>) {
   data class ProviderConfig(val clientId: String, val clientSecret: String, val authorizationUri: String, val scope: String)
 }
