ebfc7c03a73dc748a882d1752346137bac72b22b
MinChanJu <mcj00220077@gmail.com>
2026-08-07 14:26:38 +0900

    refactor: KAN-34-#48 tsconfig 및 vite 설정 개선
    - tsconfig의 경로 별칭을 세분화하여 가독성 향상
    - vite 설정에서 path alias 제거 및 tsconfigPaths 사용

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,13 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    tsconfigPaths: true,
   },
   test: {
     globals: true,
