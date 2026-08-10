053e6e9269c774cc4723de1be2d5b0e731e9fa7d
MinChanJu <mcj00220077@gmail.com>
2026-08-10 11:29:39 +0900

    refactor: KAN-36-#54 Layout 컴포넌트 구조 변경

--- apps/client/src/app/router/Layout.tsx
+++ apps/client/src/app/router/Layout.tsx
@@ -6,8 +6,10 @@ const Layout = () => {
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <main className="mx-auto min-h-0 w-full max-w-7xl grow scrollbar-thin overflow-y-auto px-4 pt-10">
-        <Outlet />
+      <main className="grow scrollbar-thin overflow-y-auto">
+        <div className="mx-auto min-h-0 w-full max-w-7xl px-4 pt-10">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
