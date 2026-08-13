b2bef55f91a247e24be8709ebbe3679dee44d30a
MinChanJu <mcj00220077@gmail.com>
2026-08-13 18:30:40 +0900

    docs: KAN-39-#57 더미 데이터 반환에 대한 설명 추가

--- apps/client/src/entities/concert/model/concert.queries.ts
+++ apps/client/src/entities/concert/model/concert.queries.ts
@@ -3,6 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import { CONCERT_QUERY_KEYS } from "./concert.constants";
 import { DAILY_RANKINGS, DUMMY_CONCERTS, HOT_CONCERTS, UPCOMING_CONCERTS } from "./dummy-concert.constants";
 
+/**
+ * 현재 API 서버가 미구현 상태이므로 더미 데이터를 반환하도록 구현되어 있습니다.
+ * API 서버가 구현되면 queryFn을 실제 API 호출로 교체하면 됩니다.
+ */
+
 export const useConcerts = () =>
   useQuery({
     queryKey: CONCERT_QUERY_KEYS.all,
