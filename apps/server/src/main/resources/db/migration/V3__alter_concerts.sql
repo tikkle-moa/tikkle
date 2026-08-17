18d28e971ca45a1bf969a325a08253ea4703e0d5
wodud1107 <wodud7992@naver.com>
2026-08-17 16:22:07 +0900

    docs: KAN-48-#76 Concert 스키마에 주석 추가

--- apps/server/src/main/resources/db/migration/V3__alter_concerts.sql
+++ apps/server/src/main/resources/db/migration/V3__alter_concerts.sql
@@ -1,5 +1,8 @@
+-- V3__alter_concerts.sql
+-- concerts 테이블에 콘서트 장르 컬럼 추가
+
 ALTER TABLE concerts
-    ADD COLUMN genre ENUM(
+ADD COLUMN genre ENUM(
     'BALLAD',
     'ROCK_METAL',
     'RAP_HIPHOP',
