f138390215d2751fb744f152bc14b4f5a925ba28
MinChanJu <mcj00220077@gmail.com>
2026-08-18 14:50:59 +0900

    feat: KAN-55-#84 CreateConcertRequest 및 UpdateConcertRequest 유효성 검사 추가
    - CreateConcertRequest의 유효성 검사 메시지 수정
    - UpdateConcertRequest에 필드 추가 및 유효성 검사 메시지 수정
    - 새로운 의존성 추가

--- apps/server/src/main/kotlin/com/example/server/concert/dto/UpdateConcertRequest.kt
+++ apps/server/src/main/kotlin/com/example/server/concert/dto/UpdateConcertRequest.kt
@@ -1,47 +1,26 @@
 package com.example.server.concert.dto
 
 import com.example.server.concert.types.ConcertGenre
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonSetter
-import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
+import org.openapitools.jackson.nullable.JsonNullable
 
 class UpdateConcertRequest(
-  @field:Size(min = 1, max = 100, message = "콘서트 제목은 1자 이상 100자 이하로 입력해야 합니다.")
-  val title: String? = null,
+  @field:NotBlank(message = "콘서트 제목은 빈 문자열이 될 수 없습니다.")
+  @field:Size(max = 100, message = "콘서트 제목은 최대 100자까지 입력할 수 있습니다.")
+  var title: JsonNullable<String> = JsonNullable.undefined(),
 
-  val genre: ConcertGenre? = null,
+  var genre: JsonNullable<ConcertGenre> = JsonNullable.undefined(),
 
-  @field:Size(min = 1, max = 100, message = "공연 장소는 1자 이상 100자 이하로 입력해야 합니다.")
-  val placeName: String? = null,
-) {
-  @field:Size(min = 1, max = 400, message = "포스터 URL은 1자 이상 400자 이하로 입력해야 합니다.")
-  var posterUrl: String? = null
-    private set
+  @field:NotBlank(message = "공연 장소는 빈 문자열이 될 수 없습니다.")
+  @field:Size(max = 100, message = "공연 장소는 최대 100자까지 입력할 수 있습니다.")
+  var placeName: JsonNullable<String> = JsonNullable.undefined(),
 
+  @field:NotBlank(message = "공연 포스터 URL은 빈 문자열이 될 수 없습니다.")
+  @field:Size(max = 400, message = "포스터 URL은 최대 400자까지 입력할 수 있습니다.")
+  var posterUrl: JsonNullable<String?> = JsonNullable.undefined(),
+
+  @field:NotBlank(message = "콘서트 설명은 빈 문자열이 될 수 없습니다.")
   @field:Size(max = 10_000, message = "콘서트 설명은 최대 10,000자까지 입력할 수 있습니다.")
-  var description: String? = null
-    private set
-
-  @get:JsonIgnore
-  @get:Schema(hidden = true)
-  var hasPosterUrl: Boolean = false
-    private set
-
-  @get:JsonIgnore
-  @get:Schema(hidden = true)
-  var hasDescription: Boolean = false
-    private set
-
-  @JsonSetter("posterUrl")
-  fun updatePosterUrl(value: String?) {
-    posterUrl = value
-    hasPosterUrl = true
-  }
-
-  @JsonSetter("description")
-  fun updateDescription(value: String?) {
-    description = value
-    hasDescription = true
-  }
-}
+  var description: JsonNullable<String?> = JsonNullable.undefined(),
+)
