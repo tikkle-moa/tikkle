db4df5adba4a5a57e663d74b66de1cc5f813ddd6
MinChanJu <mcj00220077@gmail.com>
2026-08-19 22:02:09 +0900

    feat: KAN-56-#85 공연 응답 타입에 CreateConcertRequest 추가 및 속성 수정

--- apps/client/src/entities/concert/index.ts
+++ apps/client/src/entities/concert/index.ts
@@ -1,4 +1,11 @@
 export { CONCERT_GENRE_MAP, BOOKING_STATUS_MAP } from "./model/concert.constants";
 export { getBookingStatus, getPeriod } from "./model/concert.utils";
-export type { ConcertGenreItem, BookingStatus, ConcertResponse, PerformanceResponse } from "./model/concert.types";
+export type {
+  ConcertGenre,
+  ConcertGenreItem,
+  BookingStatus,
+  ConcertResponse,
+  PerformanceResponse,
+  CreateConcertRequest,
+} from "./model/concert.types";
 export { useConcerts, useHotConcerts, useDailyRankings, useUpcomingConcerts } from "./model/concert.queries";
