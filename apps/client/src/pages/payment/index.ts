07a1d1acbdee2d309d91915409edeb9353330158
wodud1107 <wodud7992@naver.com>
2026-09-16 00:15:30 +0900

    feat: KAN-101-#168 결제 준비 화면 추가

--- apps/client/src/pages/payment/index.ts
+++ apps/client/src/pages/payment/index.ts
@@ -1,4 +1,5 @@
 export { default as PaymentPage } from "./ui/PaymentPage";
 export { default as PaymentFixtureCheckoutPage } from "./ui/PaymentFixtureCheckoutPage";
+export { default as PaymentCheckoutPage } from "./ui/PaymentCheckoutPage";
 export { default as PaymentFailPage } from "./ui/PaymentFailPage";
 export { default as PaymentSuccessPage } from "./ui/PaymentSuccessPage";
